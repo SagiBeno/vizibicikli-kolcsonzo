ca17da579f1a5cfc146231a5171b00fec7e3a310
SagiBeno <benosagi321@gmail.com>
2025-09-19 08:00:05 +0200

    Solve task 2-3.

--- src/main/java/org/example/Kolcsonzes.java
+++ src/main/java/org/example/Kolcsonzes.java
@@ -1,5 +1,58 @@
 package org.example;
 
 public class Kolcsonzes {
+    private String nev;
+    private char jarmu;
+    private int elvitelOra;
+    private int elvitelPerc;
+    private int VisszaOra;
+    private int visszaPer;
 
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public char getJarmu() {
+        return jarmu;
+    }
+
+    public void setJarmu(char jarmu) {
+        this.jarmu = jarmu;
+    }
+
+    public int getElvitelOra() {
+        return elvitelOra;
+    }
+
+    public void setElvitelOra(int elvitelOra) {
+        this.elvitelOra = elvitelOra;
+    }
+
+    public int getElvitelPerc() {
+        return elvitelPerc;
+    }
+
+    public void setElvitelPerc(int elvitelPerc) {
+        this.elvitelPerc = elvitelPerc;
+    }
+
+    public void setVisszaOra(int visszaOra) {
+        VisszaOra = visszaOra;
+    }
+
+    public int getVisszaOra() {
+        return VisszaOra;
+    }
+
+    public int getVisszaPer() {
+        return visszaPer;
+    }
+
+    public void setVisszaPer(int visszaPer) {
+        this.visszaPer = visszaPer;
+    }
 }
